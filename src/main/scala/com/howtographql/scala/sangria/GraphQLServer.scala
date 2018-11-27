03ed656368e582274f89af38a739a00f1daa580b
Oleksandr Martyshchenko <ketler13@yandex.ru>
2018-11-27 07:29:02 +0200

    Initial commit

---
+++ src/main/scala/com/howtographql/scala/sangria/GraphQLServer.scala
@@ -0,0 +1,62 @@
+package com.howtographql.scala.sangria
+
+import akka.http.scaladsl.server.Route
+import sangria.parser.QueryParser
+import spray.json.{JsObject, JsString, JsValue}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+import sangria.ast.Document
+import sangria.execution.{ExceptionHandler => EHandler, _}
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import sangria.marshalling.sprayJson._
+import com.howtographql.scala.sangria.models.{AuthenticationException, AuthorizationException}
+import com.sun.xml.internal.ws.handler.HandlerException
+
+object GraphQLServer{
+  private val dao = DBSchema.createDatabase
+
+  val ErrorHandler = EHandler {
+    case (_, AuthenticationException(message)) ⇒ HandledException(message)
+    case (_, AuthorizationException(message)) ⇒ HandledException(message)
+  }
+
+  def endpoint(requestJSON: JsValue)(implicit ec: ExecutionContext): Route = {
+    val JsObject(fields) = requestJSON
+    val JsString(query) = fields("query")
+
+    QueryParser.parse(query) match {
+      case Success(queryAst) =>
+        val operation = fields.get("operationName") collect {
+          case JsString(op) => op
+        }
+
+        val variables = fields.get("variables") match {
+          case Some(obj: JsObject) => obj
+          case _ => JsObject.empty
+        }
+        complete(executeGraphQLQuery(queryAst, operation, variables))
+      case Failure(error) =>
+          complete(BadRequest, JsObject("error" -> JsString(error.getMessage)))
+    }
+  }
+
+  private def executeGraphQLQuery(query: Document, operation: Option[String], vars: JsObject)(implicit ex: ExecutionContext) = {
+    Executor.execute(
+      GraphQLSchema.SchemaDefinition,
+      query,
+      MyContext(dao),
+      variables = vars,
+      operationName = operation,
+      deferredResolver = GraphQLSchema.Resolver,
+      exceptionHandler = ErrorHandler,
+      middleware = AuthMiddleware :: Nil
+    ).map(OK -> _)
+      .recover {
+        case error: QueryAnalysisError => BadRequest -> error.resolveError
+        case error: ErrorWithResolver => InternalServerError -> error.resolveError
+      }
+  }
+}
